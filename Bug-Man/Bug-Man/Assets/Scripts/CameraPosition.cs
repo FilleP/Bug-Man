af369c359b5405db4abc8b126dd6bdc6abe2f81c
FilleP <35960623+FilleP@users.noreply.github.com>
2018-02-01 09:57:50 +0100

    UI + mer på GameManager
    Start på UI för spelet

--- Bug-Man/Bug-Man/Assets/Scripts/CameraPosition.cs
+++ Bug-Man/Bug-Man/Assets/Scripts/CameraPosition.cs
@@ -1,54 +1,17 @@
 ﻿using UnityEngine;
 
-// Place this script on Main Camera
-
+        // place script on Main Camera
 public class CameraPosition : MonoBehaviour
 {
-    public bool lockCursor;
-    public float mouseSensitivity = 2f;
-
     public Transform target;
-    public float distanceFromTarget = 4f;
-    public Vector2 rotationYMinMax = new Vector2(-40, 85);
-    public float smoothRotationTime = 0.10f;
-    Vector3 smoothRotationVelocity;
-    Vector3 currentRotation;
+    public Vector3 offset;
+    public float smoothSpeed = 0.125f;
+    
 
-    float rotationX;
-    float rotationY;
-
-    private void Start()
+    void FixedUpdate()
     {
-        // if lockCursor is true
-        if (lockCursor)
-        {
-            // Lock cursor to be at middle of game window, and make it invisible
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-            // cursor can be visible if pressed esc and move cursor out of game window
-        }
-    }
-
-    void LateUpdate()
-    {
-        // rotationX adds up from the x position of mouse * mouse sensitivity
-        rotationX += Input.GetAxis("Mouse X") * mouseSensitivity;
-
-        // rotationY subtracts from the y position of mouse * mouse sensitivity
-        rotationY -= Input.GetAxis("Mouse Y") * mouseSensitivity;
-
-        // rotationY can't go higher or lower than rotationYMinMax variable
-        rotationY = Mathf.Clamp(rotationY, rotationYMinMax.x, rotationYMinMax.y);
-
-        // your rotation = Vector3 that changes the goal over time smoothly (your rotation, Vector3 for target, velocity of rotation, 
-        // smooth time for rotation)
-        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(rotationY, rotationX), ref smoothRotationVelocity, smoothRotationTime);
-
-        // eulerangles = your rotation, calculated above
-        transform.eulerAngles = currentRotation;
-
-        // camera position = player position - forward momentum * your distance from player
-        transform.position = target.position - transform.forward * distanceFromTarget;
+        Vector3 Position = target.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, Position, smoothSpeed);
+        transform.position = smoothPosition;
     }
 }
-
--- Bug-Man/Bug-Man/Assets/Scripts/GameManager.cs
+++ Bug-Man/Bug-Man/Assets/Scripts/GameManager.cs
@@ -11,7 +11,9 @@ public class GameManager : MonoBehaviour
     public static GameManager instance = null;
 
     private Text levelText;
-    private int level = 1;
+    private GameObject levelImage;
+    private int level = 0;
+    private bool doingSetup;
 
     private void Awake()
     {
@@ -21,7 +23,7 @@ public class GameManager : MonoBehaviour
         }
         else if (instance != this)
         {
-            Destroy(gameObject); 
+            Destroy(gameObject);
         }
 
         DontDestroyOnLoad(gameObject);
@@ -30,7 +32,27 @@ public class GameManager : MonoBehaviour
     private void OnLevelWasLoaded(int index)
     {
         level++;
+
+        InitGame();
     }
+
+    void InitGame()
+    {
+        doingSetup = true;
+
+        levelImage = GameObject.Find("LevelImage");
+        levelText = GameObject.Find("LevelText").GetComponent<Text>();
+        levelText.text = "Level" + level;
+        levelImage.SetActive(true);
+        Invoke("HideLevelImage", startDelay);
+    }
+
+    private void HideLevelImage()
+    {
+        levelImage.SetActive(false);
+        doingSetup = false;
+    }
+
     void Start()
     {
 
